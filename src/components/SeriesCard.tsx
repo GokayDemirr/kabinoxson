d6216f88dcde9f53d0e9809d6ad9e624ccf41009
GokayDemirr <gokaydemirpc@gmail.com>
2024-11-30 22:52:15 +0300

    img fix

--- src/components/SeriesCard.tsx
+++ src/components/SeriesCard.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Image from "next/image"; // Next.js Image bileşeni
 
 interface SeriesCardProps {
   id: string;
@@ -17,17 +16,7 @@ const SeriesCard: React.FC<SeriesCardProps> = ({
   return (
     <Link href={`/series/${seriesName}`}>
       <div className="rounded-lg flex flex-col items-center cursor-pointer">
-        <div className="relative w-full h-64 mb-2">
-          {/* Resim konteyneri: Boyutlar burada belirlenir */}
-          <Image
-            src={imageUrl}
-            alt={seriesName}
-            layout="fill" // Tam genişlik ve yükseklik
-            objectFit="cover" // Resmi kapsayacak şekilde boyutlandırır
-            className="rounded-md"
-            priority // Gerekirse öncelik verir
-          />
-        </div>
+        <img src={imageUrl} alt={seriesName} className="rounded-md mb-2" />
         <h2 className="tracking-wider text-2xl font-light">{seriesName}</h2>
         <p className="sm:text-base text-gray-700 text-sm ">
           {seriesDescription}
